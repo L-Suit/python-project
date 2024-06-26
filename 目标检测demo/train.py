726526afba29067e6eba59a4d32b61ce4adaed18
lsh <li78789327@qq.com>
2024-06-26 10:22:22 +0800

    fix

--- 目标检测demo/train.py
+++ 目标检测demo/train.py
@@ -1,5 +1,7 @@
 from ultralytics.models.yolo.detect import DetectionTrainer
 
-args = dict(model='yolov8n.pt', data='mydataset-IP102.yaml', epochs=5)
-trainer = DetectionTrainer(overrides=args)
-trainer.train()
+
+if __name__ == '__main__':
+    args = dict(model='yolov8n.pt', data='mydataset-IP102.yaml', epochs=5,batch=2)
+    trainer = DetectionTrainer(overrides=args)
+    trainer.train()
