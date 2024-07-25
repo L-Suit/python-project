190fe267f90ac85a5cdd6ae043a917e2e3df349e
lsh <li78789327@163.com>
2024-07-25 16:03:29 +0800

    add mmdec

--- test/test01.py
+++ test/test01.py
@@ -1,6 +1,13 @@
-from mmdet.apis import init_detector, inference_detector
+from mmdet.apis import init_detector, inference_detector,DetInferencer
 
-config_file = '../data/rtmdet_tiny_8xb32-300e_coco.py'
-checkpoint_file = '../data/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
-model = init_detector(config_file, checkpoint_file, device='cuda:0')  # or device='cuda:0'
-inference_detector(model, '../data/img/cat1.jpg')
+# config_file = '../data/rtmdet_tiny_8xb32-300e_coco.py'
+# checkpoint_file = '../data/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth'
+# model = init_detector(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
+# inference_detector(model, '../data/img/deng.png')
+
+
+# 初始化模型
+inferencer = DetInferencer('rtmdet_tiny_8xb32-300e_coco')
+
+# 推理示例图片
+inferencer('../data/img/deng.png', show=True)
