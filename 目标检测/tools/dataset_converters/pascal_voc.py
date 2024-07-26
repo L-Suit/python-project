c586d20ff9c308a4165b68b155d608860f6974d1
lsh <li78789327@163.com>
2024-07-26 14:38:58 +0800

    fix mmdetection

--- 目标检测/tools/dataset_converters/pascal_voc.py
+++ 目标检测/tools/dataset_converters/pascal_voc.py
@@ -9,8 +9,112 @@ from mmengine.utils import mkdir_or_exist, track_progress
 
 from mmdet.evaluation import voc_classes
 
+# 原始voc的类别标签
 label_ids = {name: i for i, name in enumerate(voc_classes())}
 
+# label_ids = { 0: 'rice leaf roller',
+#               1: 'rice leaf caterpillar',
+#               2: 'paddy stem maggot',
+#               3: 'asiatic rice borer',
+#               4: 'yellow rice borer',
+#               5: 'rice gall midge',
+#               6: 'Rice Stemfly',
+#               7: 'brown plant hopper',
+#               8: 'white backed plant hopper',
+#               9: 'small brown plant hopper',
+#               10: 'rice water weevil',
+#               11: 'riceleafhopper',
+#               12: 'grain spreader thrips',
+#               13: 'rice shell pest',
+#               14: 'grub',
+#               15: 'mole cricket',
+#               16: 'wireworm',
+#               17: 'white margined moth',
+#               18: 'black cutworm',
+#               19: 'large cutworm',
+#               20: 'yellow cutworm',
+#               21: 'red spider',
+#               22: 'corn borer',
+#               23: 'army worm',
+#               24: 'aphids',
+#               25: 'Potosiabre vitarsis',
+#               26: 'peach borer',
+#               27: 'english grain aphid',
+#               28: 'green bug',
+#               29: 'bird cherry-oataphid',
+#               30: 'wheat blossom midge',
+#               31: 'penthaleus major',
+#               32: 'longlegged spider mite',
+#               33: 'wheat phloeothrips',
+#               34: 'wheat sawfly',
+#               35: 'cerodonta denticornis',
+#               36: 'beet fly',
+#               37: 'flea beetle',
+#               38: 'cabbage army worm',
+#               39: 'beet army worm',
+#               40: 'Beet spot flies',
+#               41: 'meadow moth',
+#               42: 'beet weevil',
+#               43: 'sericaorient alismots chulsky',
+#               44: 'alfalfa weevil',
+#               45: 'flax budworm',
+#               46: 'alfalfa plant bug',
+#               47: 'tarnished plant bug',
+#               48: 'Locustoidea',
+#               49: 'lytta polita',
+#               50: 'legume blister beetle',
+#               51: 'blister beetle',
+#               52: 'therioaphis maculata Buckton',
+#               53: 'odontothrips loti',
+#               54: 'Thrips',
+#               55: 'alfalfa seed chalcid',
+#               56: 'Pieris canidia',
+#               57: 'Apolygus lucorum',
+#               58: 'Limacodidae',
+#               59: 'Viteus vitifoliae',
+#               60: 'Colomerus vitis',
+#               61: 'Brevipoalpus lewisi McGregor',
+#               62: 'oides decempunctata',
+#               63: 'Polyphagotars onemus latus',
+#               64: 'Pseudococcus comstocki Kuwana',
+#               65: 'parathrene regalis',
+#               66: 'Ampelophaga',
+#               67: 'Lycorma delicatula',
+#               68: 'Xylotrechus',
+#               69: 'Cicadella viridis',
+#               70: 'Miridae',
+#               71: 'Trialeurodes vaporariorum',
+#               72: 'Erythroneura apicalis',
+#               73: 'Papilio xuthus',
+#               74: 'Panonchus citri McGregor',
+#               75: 'Phyllocoptes oleiverus ashmead',
+#               76: 'Icerya purchasi Maskell',
+#               77: 'Unaspis yanonensis',
+#               78: 'Ceroplastes rubens',
+#               79: 'Chrysomphalus aonidum',
+#               80: 'Parlatoria zizyphus Lucus',
+#               81: 'Nipaecoccus vastalor',
+#               82: 'Aleurocanthus spiniferus',
+#               83: 'Tetradacus c Bactrocera minax',
+#               84: 'Dacus dorsalis(Hendel)',
+#               85: 'Bactrocera tsuneonis',
+#               86: 'Prodenia litura',
+#               87: 'Adristyrannus',
+#               88: 'Phyllocnistis citrella Stainton',
+#               89: 'Toxoptera citricidus',
+#               90: 'Toxoptera aurantii',
+#               91: 'Aphis citricola Vander Goot',
+#               92: 'Scirtothrips dorsalis Hood',
+#               93: 'Dasineura sp',
+#               94: 'Lawana imitata Melichar',
+#               95: 'Salurnis marginella Guerr',
+#               96: 'Deporaus marginatus Pascoe',
+#               97: 'Chlumetia transversa',
+#               98: 'Mango flat beak leafhopper',
+#               99: 'Rhytidodera bowrinii white',
+#               100: 'Sternochetus frigidus',
+#               101: 'Cicadellidae'
+# }
 
 def parse_xml(args):
     xml_path, img_path = args
@@ -24,8 +128,8 @@ def parse_xml(args):
     bboxes_ignore = []
     labels_ignore = []
     for obj in root.findall('object'):
-        name = obj.find('name').text
-        label = label_ids[name]
+        # name = obj.find('name').text
+        label = obj.find('name').text
         difficult = int(obj.find('difficult').text)
         bnd_box = obj.find('bndbox')
         bbox = [
@@ -83,7 +187,7 @@ def cvt_annotations(devkit_path, years, split, out_file):
             for img_name in img_names
         ]
         img_paths = [
-            f'VOC{year}/JPEGImages/{img_name}.jpg' for img_name in img_names
+            f'VOC{year}/images/{img_name}.jpg' for img_name in img_names
         ]
         part_annotations = track_progress(parse_xml,
                                           list(zip(xml_paths, img_paths)))
@@ -221,7 +325,8 @@ def main():
             prefix = 'voc12'
         elif year == ['2007', '2012']:
             prefix = 'voc0712'
-        for split in ['train', 'val', 'trainval']:
+        # for split in ['train', 'val', 'trainval']:
+        for split in ['trainval','test']:
             dataset_name = prefix + '_' + split
             print(f'processing {dataset_name} ...')
             cvt_annotations(devkit_path, year, split,
