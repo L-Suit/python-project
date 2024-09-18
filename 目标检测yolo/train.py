4ef333c980c92bab387952dd16c2898a561c5f86
carlos <caigy314@163.com>
2024-09-18 14:59:30 +0800

    add 服务器路径

--- 目标检测yolo/train.py
+++ 目标检测yolo/train.py
@@ -2,6 +2,6 @@ from ultralytics.models.yolo.detect import DetectionTrainer
 
 
 if __name__ == '__main__':
-    args = dict(model='yolov8n.pt', data='mydataset-IP102.yaml', epochs=5, batch=64, workers=2)
+    args = dict(model='yolov8n.pt', data='mydataset-IP102.yaml', epochs=100, batch=32, workers=4)
     trainer = DetectionTrainer(overrides=args)
     trainer.train()
