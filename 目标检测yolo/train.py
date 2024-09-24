21923aa9bd2e89f02d4d4108df83c2d4bb7c7b96
lsh <li78789327@qq.com>
2024-09-24 16:27:13 +0800

    fix

--- 目标检测yolo/train.py
+++ 目标检测yolo/train.py
@@ -1,8 +1,13 @@
 from ultralytics.models.yolo.detect import DetectionTrainer
+from ultralytics.utils import DEFAULT_CFG
 
 
 if __name__ == '__main__':
-    args = dict(model='yolov8n.pt', data='mydataset-forest-31pest.yaml', epochs=10, batch=16, workers=2)
+    args = dict(model='yolov8n.pt', data='mydataset-forest-31pest.yaml', epochs=20, batch=16, workers=2)
     # data='mydataset-IP102.yaml'  'mydataset-forest-31pest.yaml'
+
+    # 设置模型保存路径
+    DEFAULT_CFG.save_dir = f"runs/detect/yolov8n_31pests"
+
     trainer = DetectionTrainer(overrides=args)
     trainer.train()
