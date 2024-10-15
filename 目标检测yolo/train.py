8d318e6d8a1d98d5835d99783d8e3523fd01ff79
carlos <caigy314@163.com>
2024-10-15 10:44:47 +0800

    fix

--- 目标检测yolo/train.py
+++ 目标检测yolo/train.py
@@ -2,6 +2,6 @@ from ultralytics.models.yolo.detect import DetectionTrainer
 
 
 if __name__ == '__main__':
-    args = dict(model='yolov8n.pt', data='mydataset-IP102.yaml', epochs=100, batch=32, workers=4)
+    args = dict(model='yolov8n.pt', data='mydataset-林业害虫31类.yaml', epochs=100, batch=32, workers=4)
     trainer = DetectionTrainer(overrides=args)
     trainer.train()
