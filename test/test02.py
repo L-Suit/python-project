15edcce0636980551f4d24de61838ba2fabd9494
lsh <li78789327@163.com>
2024-10-23 11:59:51 +0800

    add v8魔改模块

--- test/test02.py
+++ test/test02.py
@@ -8,5 +8,8 @@ if image is None:
     print('Error: Image not found or unable to read')
 
 cv2.imshow('image', image)
+# 等比例缩放图片到480*480像素
+resized_image = cv2.resize(image, (480, 480))
+cv2.imshow('resized_image', resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
