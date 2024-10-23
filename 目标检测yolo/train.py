bde41e5fb4113abe16909205e130062a17a3ee8e
lsh <li78789327@qq.com>
2024-10-23 11:20:24 +0800

    fix

--- 目标检测yolo/train.py
+++ 目标检测yolo/train.py
@@ -15,7 +15,7 @@ from ultralytics.utils import DEFAULT_CFG
 
 
 if __name__ == '__main__':
-    model = YOLO(r'./cfg/models/v8/yolov8-weather-GDIP.yaml')
+    model = YOLO(r'D:\Pycharm-project\pythonProject\目标检测yolo\runs\detect\yolov8n_ip02weather_epo100_AdamW\weights\last.pt')
     # model.load('yolov8n.pt') # loading pretrain weights
     epoch = 100
     batch = 8
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
 
 
-    model.train(data=r'mydataset-forest-31pest.yaml',
+    model.train(data=r'mydataset-IP102.yaml',
                 # 如果大家任务是其它的'ultralytics/cfg/default.yaml'找到这里修改task可以改成detect, segment, classify, pose
                 cache=True,
                 imgsz=640,
@@ -38,9 +38,9 @@ if __name__ == '__main__':
                 optimizer=optimizer, # 优化器设置
                 lr0=lr0,
                 momentum=momentum,
-                # resume=True, # 如过想续训,此处设置true，model不用.yaml改为last.pt的位置
+                resume=True, # 如过想续训,此处设置true，model不用.yaml改为last.pt的位置
                 amp=True,  # 如果出现训练损失为Nan可以关闭amp
                 # half=True,
                 project='runs/detect',
-                name=f'yolov8n_GDIP_for31weather_epo{epoch}_{optimizer}',
+                name=f'yolov8n_ip02weather_epo{epoch}_{optimizer}',
                 )
